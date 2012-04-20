5e32d2a0c62e1893b6e1e81a5e70081ff7f52bb2
markiros <markiros@gmail.com>
2012-04-20 23:52:42 +0400

    Added ignored config

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
-
-$input = isset($_GET['name']) ? $_GET['name'] : 'World';
-
-header('Content-Type: text/html; charset=utf-8');
-
-printf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8'));
+ 
+// framework/index.php
+ 
+require_once __DIR__.'/autoload.php';
+ 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+ 
+$request = Request::createFromGlobals();
+ 
+$input = $request->get('name', 'World');
+ 
+$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+ 
+$response->send();
