87412a64b321536d8ca7f130678ab8b62b7c8bd0
markiros <markiros@gmail.com>
2012-04-20 23:44:03 +0400

    Added first unit test

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
 <?php
 
-// framework/index.php
+$input = isset($_GET['name']) ? $_GET['name'] : 'World';
 
-require_once __DIR__.'/autoload.php';
+header('Content-Type: text/html; charset=utf-8');
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-$request = Request::createFromGlobals();
-
-$input = $request->get('name', 'World');
-
-$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
-
-$response->send();
+printf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8'));
