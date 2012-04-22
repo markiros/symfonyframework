dae826e79037fedd03e5f2f9e39b2039815ae802
Markiros <markiros@gmail.com>
2012-04-22 22:07:44 +0400

    Complete part-8 of tutorial

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -9,13 +9,15 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
 
 class Framework
 {
     protected $matcher;
     protected $resolver;
 
-    public function __construct(UrlMatcher $matcher, ControllerResolver $resolver)
+    public function __construct(UrlMatcherInterface $matcher, ControllerResolverInterface $resolver)
     {
         $this->matcher = $matcher;
         $this->resolver = $resolver;
