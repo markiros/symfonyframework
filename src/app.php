edad9bf04a71deba9a13d8968e0a8cf030ad5bae
Markiros <markiros@gmail.com>
2012-04-22 16:44:50 +0400

    Finished part 6

--- src/app.php
+++ src/app.php
@@ -13,6 +13,18 @@ function is_leap_year($year = null) {
     return 0 == $year % 400 || (0 == $year % 4 && 0 != $year % 100);
 }
 
+class LeapYearController
+{
+    public function indexAction($year)
+    {
+        if (is_leap_year($year)) {
+            return new Response('Yep, this is a leap year!');
+        }
+
+        return new Response('Nope, this is not a leap year.');
+    }
+}
+
 $routes = new Routing\RouteCollection();
 $routes->add('bye', new Routing\Route('/bye'));
 $routes->add('hello', new Routing\Route('/hello/{name}', array(
@@ -31,13 +43,7 @@ $routes->add('hello', new Routing\Route('/hello/{name}', array(
 )));
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
     'year' => null,
-    '_controller' => function ($request) {
-        if (is_leap_year($request->attributes->get('year'))) {
-            return new Response('Yep, this is a leap year!');
-        }
-
-        return new Response('Nope, this is not a leap year.');
-    }
+    '_controller' => 'LeapYearController::indexAction',
 )));
 
 return $routes;
