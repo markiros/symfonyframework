a88779534d1bd73e23d249c33287ebcb8c16b793
markiros <markiros@gmail.com>
2012-04-22 12:06:02 +0400

    Temp save

---
+++ hello.php
@@ -0,0 +1,6 @@
+<?php
+
+// framework/hello.php
+
+$input = $request->get('name', 'World');
+$response->setContent(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
--- index.php
+++ index.php
@@ -1,16 +1,10 @@
 <?php
- 
+
 // framework/index.php
- 
-require_once __DIR__.'/autoload.php';
- 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
- 
-$request = Request::createFromGlobals();
- 
+
+require_once __DIR__.'/init.php';
+
 $input = $request->get('name', 'World');
- 
-$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
- 
+
+$response->setContent(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
 $response->send();
